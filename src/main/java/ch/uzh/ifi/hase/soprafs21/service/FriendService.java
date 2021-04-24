8744db45f9a1aca99f984d129a5d3a5c22f6b91a
Denis <treskdenis@gmail.com>
2021-04-24 22:46:59 +0200

    hot fixes for friend list and db properties

--- src/main/java/ch/uzh/ifi/hase/soprafs21/service/FriendService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/service/FriendService.java
@@ -29,12 +29,14 @@ public class FriendService {
     }
 
     public List<User> getFriends(User a){
-        if(a.getFriends() != null){
-            List<User> friends = new ArrayList<>(a.getFriends());
-            friends.addAll(a.getfriendOf()); 
-            return  friends;
+        public List<User> getFriends(User a){
+            if(a.getFriends() != null){
+                List<User> friends = new ArrayList<>(a.getFriends());
+                friends.addAll(a.getfriendOf()); 
+                return  friends;
+            }
+            return new ArrayList<>();
         }
-        return new ArrayList<>();
     }
 
     public void addFriends(User a, User b){
