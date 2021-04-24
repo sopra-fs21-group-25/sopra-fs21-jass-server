f478de0ff778f74fe901cf77fae3013048728a00
Denis <treskdenis@gmail.com>
2021-04-24 22:53:54 +0200

    hot fix of a hot fix

--- src/main/java/ch/uzh/ifi/hase/soprafs21/service/FriendService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/service/FriendService.java
@@ -29,14 +29,12 @@ public class FriendService {
     }
 
     public List<User> getFriends(User a){
-        public List<User> getFriends(User a){
-            if(a.getFriends() != null){
-                List<User> friends = new ArrayList<>(a.getFriends());
-                friends.addAll(a.getfriendOf()); 
-                return  friends;
-            }
-            return new ArrayList<>();
+        if(a.getFriends() != null){
+            List<User> friends = new ArrayList<>(a.getFriends());
+            friends.addAll(a.getfriendOf()); 
+            return  friends;
         }
+        return new ArrayList<>();
     }
 
     public void addFriends(User a, User b){
