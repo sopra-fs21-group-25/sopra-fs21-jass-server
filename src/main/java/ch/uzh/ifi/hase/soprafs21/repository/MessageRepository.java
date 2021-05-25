3bcf38e3fd662bd56a8ee48df7a0862897105c15
Denis <treskdenis@gmail.com>
2021-05-25 23:15:43 +0200

    scheduler to clean old messages

--- src/main/java/ch/uzh/ifi/hase/soprafs21/repository/MessageRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/repository/MessageRepository.java
@@ -3,10 +3,17 @@ package ch.uzh.ifi.hase.soprafs21.repository;
 import ch.uzh.ifi.hase.soprafs21.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.UUID;
+import java.util.Date;
 
 @Repository("messageRepository")
 public interface MessageRepository extends JpaRepository<Message, UUID> {
 
+    @Modifying
+    @Query(value = "DELETE FROM Message m WHERE m.timestamp < :date")
+    void removeOldMessages(@Param("date") Date creationDate);
 }
