a1e36919fabf99daaa42d1ef8bff6862b2d99c0b
Denis <treskdenis@gmail.com>
2021-05-30 22:17:35 +0200

    tests for FriendService

--- src/test/java/ch/uzh/ifi/hase/soprafs21/service/FriendServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/service/FriendServiceTest.java
@@ -38,6 +38,7 @@ public class FriendServiceTest {
     private RegisteredUser willi;
     private RegisteredUser louise;
     private RegisteredUser rebekka;
+    private RegisteredUser testUser; 
 
     @BeforeEach
     public void setup() {
@@ -54,12 +55,20 @@ public class FriendServiceTest {
         louise = new RegisteredUser();
         louise.setPassword("verySafe");
         louise.setUsername("Louise");
+        louise.setId(UUID. randomUUID());
         userService.createRegisteredUser(louise);
 
         rebekka = new RegisteredUser();
         rebekka.setPassword("verySafe");
         rebekka.setUsername("Rebekka");
         userService.createRegisteredUser(rebekka);
+
+        testUser = new RegisteredUser();
+        testUser.setPassword("testUser");
+        testUser.setUsername("testUser");
+        testUser.setId(UUID. randomUUID());
+        testUser.setFriends(new ArrayList<User>());
+        userService.createRegisteredUser(testUser);
 
         List<User> friendList = new ArrayList<>();
         friendList.add(rebekka);
@@ -85,4 +94,31 @@ public class FriendServiceTest {
         assertEquals(2, willisFriends.size());
     }
 
+    @Test
+    public void addFriend_success(){
+        List<User> testUserFriends = new ArrayList<>();
+        Mockito.doAnswer(invocation -> {
+            User arg0 = invocation.getArgument(0);
+            User arg1 = invocation.getArgument(1);
+            testUserFriends.add(arg1);
+            assertEquals(testUserFriends.get(0), louise);
+            return null;
+        }).when(friendService).addFriends(Mockito.any(User.class), Mockito.any(User.class));
+        friendService.addFriends(testUser, louise);
+    }
+
+    @Test
+    public void removeFriend_success(){
+        List<User> testUserFriends = new ArrayList<>();
+        testUserFriends.add(louise);
+        testUser.setFriends(testUserFriends);
+        Mockito.doAnswer(invocation -> {
+            User arg0 = invocation.getArgument(0);
+            User arg1 = invocation.getArgument(1);
+            testUserFriends.remove(arg1);
+            assertEquals(testUserFriends.size(), 0);
+            return null;
+        }).when(friendService).removeFriend(Mockito.any(User.class), Mockito.any(User.class));
+        friendService.removeFriend(testUser, louise);
+    }
 }
